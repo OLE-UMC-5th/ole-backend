d1b1fd8bffc9a60f3bb2e3f2bf3abc319b617012
mok-choi <hyunmok.choi@gmail.com>
2024-02-11 10:38:43 +0900

    fix: change data source
    and remove unnecessary lines

--- src/main/java/com/example/olebackend/config/SecurityConfig.java
+++ src/main/java/com/example/olebackend/config/SecurityConfig.java
@@ -26,8 +26,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
-import javax.servlet.DispatcherType;
-
 
 @Configuration
 @EnableWebSecurity
